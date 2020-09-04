c08395b668efcef9d6003311afc4b52fb460fe2f
grafijoss <grafijoss@hotmail.com>
2020-09-04 14:05:48 -0500

    axios

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+
+import axios from 'axios'
+
 import FormularioSimple from './components/FormularioSimple'
 import './App.css';
 
 class App extends Component {
 	constructor() {
 		super()
-		// GET, POST, PUT, PATCH, DELETE
 		// GET
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then(x => x.json())
-			.then(x => console.log(x))
+		axios.get('https://jsonplaceholder.typicode.com/users')
+			.then(({ data }) => console.log(data))
 		// POST
-		fetch('https://jsonplaceholder.typicode.com/users', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'aplication/json'
-			},
-			body: JSON.stringify({
-				name: 'Joss',
-				userName: 'Grafijoss'
-			})
+		axios.post('https://jsonplaceholder.typicode.com/users', {
+			name: 'Jose Montenegro',
+			userName: 'Grafijoss'
 		})
-			.then(x => x.json())
-			.then(x => console.log(x))
+			.then(({ data }) => console.log(data))
 	}
 	render() {
 		return (
