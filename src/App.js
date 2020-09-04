992025651b9cc2ce0b11066018a3e3807003bf54
grafijoss <grafijoss@hotmail.com>
2020-09-04 13:47:05 -0500

    fetch

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import FormularioSimple from './components/FormularioSimple'
 import './App.css';
 
 class App extends Component {
+	constructor() {
+		super()
+		// GET, POST, PUT, PATCH, DELETE
+		// GET
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(x => x.json())
+			.then(x => console.log(x))
+		// POST
+		fetch('https://jsonplaceholder.typicode.com/users', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'aplication/json'
+			},
+			body: JSON.stringify({
+				name: 'Joss',
+				userName: 'Grafijoss'
+			})
+		})
+			.then(x => x.json())
+			.then(x => console.log(x))
+	}
 	render() {
 		return (
 			<div className="App" >
